f52df43d09457523737a600a8deda3a588a079a4
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-24 23:41:21 +0100

    Lock is showing

--- ContentView.swift
+++ ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color(red: 83/255, green: 105/255, blue: 118/255), Color(red: 41/255, green: 43/255, blue: 73/255)]), startPoint: UnitPoint.topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+            LockView() // MARK: - Lock
         }
     }
 }
---
+++ Views/LockView.swift
@@ -0,0 +1,34 @@
+//
+//  LockView.swift
+//  LockAnimationApp
+//
+//  Created by Nicola Kaleta on 24/02/2024.
+//
+
+import SwiftUI
+
+struct LockView: View {
+    @State var isLock: Bool = false
+    
+    var body: some View {
+        ZStack {
+            LockTextAndToggleView(isLock: $isLock)
+        }
+    }
+}
+
+#Preview {
+    LockView()
+}
+
+// MARK: - LockTextAndToggle View
+struct LockTextAndToggleView : View {
+    @Binding var isLock: Bool
+    var body: some View {
+        VStack {
+            Text(isLock ? "Unlock" : "Lock")
+                .font(.largeTitle)
+                .foregroundStyle(isLock ? .green : .gray)
+        }
+    }
+}
