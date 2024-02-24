a6f2b6427feeefba577ee54ec5d843e0a9a98819
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-24 23:53:22 +0100

    it is showing circle but without animation

--- Views/LockCircleView.swift
+++ Views/LockCircleView.swift
@@ -14,6 +14,10 @@ struct LockCircleView: View {
         ZStack {
             Circle()
                 .trim(from: 0, to: 0.5)
+                .stroke(isLock ? .green : .gray, style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                .frame(width: 160, height: 160, alignment: .center)
+                .rotationEffect(.degrees(isLock ? 270 : 180))
+                .offset(x: 0, y: -133)
         }
     }
 }
